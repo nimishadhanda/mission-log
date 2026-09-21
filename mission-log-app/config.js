9706c7174889646935555a8468eeddaf0c04020f
nimishadhanda <nimishadhanda@gmail.com>
2026-09-21 16:33:37 +0530

    Update config.js

--- mission-log-app/config.js
+++ mission-log-app/config.js
@@ -3,4 +3,4 @@
 // only allowed to do what the RLS policies in supabase-schema.sql permit.
 window.SUPABASE_URL = "https://uaycmueaukhwfryeceep.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVheWNtdWVhdWtod2ZyeWVjZWVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3MDQ2MDEsImV4cCI6MjEwNDI4MDYwMX0.PKDaduA7k6cPcDeKhX3fpGjC-ABOl70k_OyiWoCyu7M";
-window.PARENT_PIN = "2468";
+window.PARENT_PIN = "2284";
